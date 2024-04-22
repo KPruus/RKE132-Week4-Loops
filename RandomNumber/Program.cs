e15fcb088b043847e7c5613aa5a08b22ae3b0767
KPruus <kristjanpruus@hotmail.com>
2024-04-22 15:59:31 +0300

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Random rnd = new Random();
+
+int myRandomNum;
+int randomSum = 0;
+
+for (int i = 0; i < 3; i++)
+{
+    myRandomNum = rnd.Next(0, 11); // 0 - 10 tegelikult
+    randomSum = randomSum + myRandomNum;
+    Console.WriteLine($"My random mumber is: {myRandomNum}");
+}
+
+Console.WriteLine($"random sum total:{randomSum}");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true; 
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    Console.WriteLine("Make a guess. What number am I thinking of, between 1-3");
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Correct, congrats!!");
+        //loopActive = false;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Nope, try again.");
+    }
+}
+
+Console.WriteLine("G'day");
